440b1f553698b6434ccf54a1be7c9e4c8b6a09fe
Pierre Kisters <pierrekisters@gmail.com>
2024-09-26 13:42:28 +0200

    set MediaType to application/problem+json in http4s module

--- http4s/src/main/scala/de/lhns/httpproblem/http4s/json.scala
+++ http4s/src/main/scala/de/lhns/httpproblem/http4s/json.scala
@@ -2,12 +2,15 @@ package de.lhns.httpproblem.http4s
 
 import cats.effect.Concurrent
 import de.lhns.httpproblem.HttpProblem
-import org.http4s.{EntityDecoder, EntityEncoder}
+import org.http4s.headers.`Content-Type`
+import org.http4s.{EntityDecoder, EntityEncoder, MediaType}
 
 object json {
-    implicit def httpProblemEncoder[F[_]]: EntityEncoder[F, HttpProblem] =
-      org.http4s.circe.jsonEncoderOf[F, HttpProblem]
+  implicit def httpProblemEncoder[F[_]]: EntityEncoder[F, HttpProblem] =
+    org.http4s.circe
+      .jsonEncoderOf[F, HttpProblem]
+      .withContentType(`Content-Type`(MediaType.application.`problem+json`))
 
-    implicit def httpProblemDecoder[F[_]: Concurrent]: EntityDecoder[F, HttpProblem] =
-      org.http4s.circe.jsonOf[F, HttpProblem]
+  implicit def httpProblemDecoder[F[_]: Concurrent]: EntityDecoder[F, HttpProblem] =
+    org.http4s.circe.jsonOf[F, HttpProblem]
 }
